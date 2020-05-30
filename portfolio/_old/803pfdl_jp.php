5dd13e881f938e8340a712e62e1a14475af15084
tomonishintaku <tomonishintaku@gmail.com>
2020-05-30 12:31:02 -0700

    add folders second

---
+++ portfolio/_old/803pfdl_jp.php
@@ -0,0 +1,3 @@
+<?php
+$pfdl_jp = '<a href="https://tomonishintaku.com/portfolio/180523_shintaku_tomoni_jp.pdf" target="_blank"><i class="far fa-file-pdf" aria-hidden="true"></i></i>作家ポートフォリオ ダウンロード<span>(PDF:11.5MB)</span></a>';
+?>
---
+++ portfolio/pfdl_jp.php
@@ -0,0 +1,4 @@
+<?php
+$pfdl_jp = '<a href="https://tomonishintaku.com/portfolio/shintakutomoni_jp.pdf" target="_blank" onclick="ga(\'send\',\'event\',\'banner\',\'click\',\'jp_portfolio\', 1);"><i class="far fa-file-pdf" aria-hidden="true"></i></i>ポートフォリオ ダウンロード<span>(PDF:15.8MB)</span></a>';
+$pfdl_raw_jp = '<a href="https://tomonishintaku.com/portfolio/shintakutomoni_raw_jp.pdf" target="_blank" onclick="ga(\'send\',\'event\',\'banner\',\'click\',\'jp_portfolio\', 1);"><i class="far fa-file-pdf" aria-hidden="true"></i></i>ポートフォリオ ダウンロード/高品質印刷用データ<span>(PDF:80.6MB)</span></a>';
+?>
