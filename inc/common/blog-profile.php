aaf1aab1e35f8092b7b8d4700f1bbb4e64fffc4c
tomonishintaku <tomonishintaku@gmail.com>
2020-08-08 21:13:31 -0700

    update 9-1

---
+++ inc/common/blog-profile.php
@@ -0,0 +1,16 @@
+<article class="profile-unit">
+    <section class="profile-unit__image" itemtype="http://schema.org/ImageObject">
+        <a href="https://tomonishintaku.com/about/" target="_blank" title="新宅 睦仁" rel="nofollow"> <img alt=""
+                src="https://"></a>
+    </section>
+    <section class="profile-unit__detail">
+        <strong class="abh_name fn name" itemprop="author" itemtype="http://schema.org/Person"><a
+                href="https://tomonishintaku.com/about/" class="url" target="_blank" rel="nofollow">新宅 睦仁</a></strong>
+        <div class="profile-unit__sns-icons">
+            <?php require_once dirname(__FILE__).'functions.php';?>
+            <?php branchURL('/en/', $sns_en, $sns); /*This function from "/inc/common/functions.php"*/?>
+        </div>
+        <p class="profile-unit__text" itemprop="description">
+            広島→福岡→東京→シンガポール→現在ロサンゼルス在住の現代美術家。美大と調理師専門学校に学んだ経験から食をテーマに作品を制作。無類の居酒屋好き。</p>
+    </section>
+</article>
--- inc/sns.php
+++ inc/sns.php
@@ -1,1 +1,17 @@
-<li class="main-sns"><a class="button twitter" href="https://twitter.com/tomonishintaku" target="_blank" title="Click to share this post on Twitter"><i class="fab fa-twitter-square" aria-hidden="true"></i></a></li><li class="main-sns"> <a class="button facebook" href="https://www.facebook.com/tomoni.shintaku" target="_blank"><i class="fab fa-facebook-square" aria-hidden="true"></i></a></li><li> <a class="button instagram" href="https://www.instagram.com/tomonishintaku/" target="_blank" title="Click to share this post on Instagram"><i class="fab fa-instagram" aria-hidden="true"></i></a></li><li> <a class="button youtube" href="https://www.youtube.com/channel/UCDnt1pKShw7vYrD1JavHBkg" target="_blank" title="Click to share this post on YouTube"><i class="fab fa-youtube-square"></i></a></li><li> <a class="button linkedin" href="https://www.linkedin.com/in/tomonishintaku/" target="_blank" title="Click to share this post on LinkedIn"><i class="fab fa-linkedin" aria-hidden="true"></i></a></li>
+<ul>
+	<li class="main-sns">
+		<a class="button twitter" href="https://twitter.com/tomonishintaku" target="_blank" title="Click to share this post on Twitter"><i aria-hidden="true" class="fab fa-twitter-square"></i></a>
+	</li>
+	<li class="main-sns">
+		<a class="button facebook" href="https://www.facebook.com/tomoni.shintaku" target="_blank"><i aria-hidden="true" class="fab fa-facebook-square"></i></a>
+	</li>
+	<li>
+		<a class="button instagram" href="https://www.instagram.com/tomonishintaku/" target="_blank" title="Click to share this post on Instagram"><i aria-hidden="true" class="fab fa-instagram"></i></a>
+	</li>
+	<li>
+		<a class="button youtube" href="https://www.youtube.com/channel/UCDnt1pKShw7vYrD1JavHBkg" target="_blank" title="Click to share this post on YouTube"><i class="fab fa-youtube-square"></i></a>
+	</li>
+	<li>
+		<a class="button linkedin" href="https://www.linkedin.com/in/tomonishintaku/" target="_blank" title="Click to share this post on LinkedIn"><i aria-hidden="true" class="fab fa-linkedin"></i></a>
+	</li>
+</ul>
