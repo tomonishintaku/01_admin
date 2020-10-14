b7516086d6606923215167ad07f3cca33269adb2
tomonishintaku <tomonishintaku@gmail.com>
2020-10-14 18:02:37 +0900

    update

--- inc/bnr.php
+++ inc/bnr.php
@@ -1,33 +1,25 @@
 				<section id="top_bnr_space" class="foot_bnr">
-<a href="https://shintaku.co/blog/taro-award-plan-apply" target="_blank" style="color: #555;display: block;padding: 1rem;border: 4px solid #c00; margin-bottom: 1rem">
-	<strong style="color: #c00;display:block;text-align:center;margin-bottom: 0.5rem;">【重要なお知らせ】</strong> 現在、国内外のIT企業で実務経験10年以上のれっきとしたプロの職業WEBデザイナーでもある新宅の作品制作の一環で、<u><strong>WEBデザイン関連業務全般を、1時間あたり米一合(約45円)の現物で請け負っております</strong></u>。詳細はWEBデザインを主業務とする個人事業 <span style="color: #00e;">www.shintaku.co</span> にてご確認ください。
-</a>
-
 					<ul class="clearfix">
 
 						<!--li><a href="https://affordableartfair.com/fairs/singapore" target="_blank"><img class="lazyload" src="/images/banner/2018/bnr_aaf-sg-2018_jp.png" alt="アフォーダブルアートフェア、Affordable Art Fair(シンガポール) 11/16〜18"></a></li>
 
 						<li><a href="/exhibition/2018_colour-me-well.html" onclick="ga('send','event','banner','click','colour2018', 1);"><img class="lazyload" src="/images/banner/bnr_colour-me-well2018_jp.png" alt="新宅睦仁個展COLOUR ME WELL/One East Asia Gallery/シンガポール"></a></li-->
 
-						<li><a href="http://ec.tagboat.com/jp/products/list.php?author_id=100265&tngs_flg=0" target="_blank" onclick="ga('send','event','banner','click','tagboat', 1);"><img class="lazyload" src="/images/top/bnr_tagboat.jpg" alt="タグボートで新宅睦仁作品を販売中"></a></li>
+						<li><a href="https://shintaku.co/" target="_blank" onclick="ga('send','event','banner','click','shintaku-web', 1);"><img class="lazyload" src="/images/banner/bnr_shintaku-web-design.png" title="現代美術家新宅睦仁のWEBデザインの個人事業SHINTAKU。" alt="WEBデザインの個人事業SHINTAKU。のWEBサイトへのリンクバナー"></a></li>
+
+						<li><a href="http://ec.tagboat.com/jp/products/list.php?author_id=100265&tngs_flg=0" target="_blank" onclick="ga('send','event','banner','click','tagboat', 1);" rel="noopener noreferrer"><img class="lazyload" src="/images/top/bnr_tagboat.jpg" alt="タグボートで新宅睦仁作品を販売中"></a></li>
 
 						<li><a href="/portfolio/" onclick="ga('send','event','banner','click','portfolio', 1);"><img class="lazyload" src="/images/top/bnr_portfolio-dl_jp.png" alt="作家ポートフォリオPDF版ダウンロード"></a></li>
 
 						<!--li><a class="lazyload"href="/works/writing.html"><img class="lazyload" src="/images/top/bnr_booklet_muron-01.png" alt="新宅睦仁ブログ本「むろん、どこにも行きたくない。悲哀奮闘篇」好評発売中"></a></li-->
 
-						<!--li><a class="lazyload"href="http://kyotojapan-artnow.net/?mode=cate&cbid=1239949&csid=35&sort=n" target="_blank" onclick="ga('send','event','banner','click','a3', 1);"><img class="lazyload" src="/images/top/bnr_voice_a3.png" alt="MATSUO MEGUMI +VOICE GALLERY pfs/w　a3プロジェクト"></a></li-->
-
-
-
-						<li><a href="http://tomonishintaku.com/blog/voice/" onclick="ga('send','event','banner','click','voice-blog', 1);"><img class="lazyload" src="/images/banner/bnr_voice-blog.png" title="現代美術家新宅睦仁の音声ブログ" alt="現代美術家新宅睦仁の音声ブログ。その日に考えていることをとりとめもなく話す。毎日更新"></a></li>
+						<!--li><a href="http://tomonishintaku.com/blog/voice/" onclick="ga('send','event','banner','click','voice-blog', 1);"><img class="lazyload" src="/images/banner/bnr_voice-blog.png" title="現代美術家新宅睦仁の音声ブログ" alt="現代美術家新宅睦仁の音声ブログ。その日に考えていることをとりとめもなく話す。毎日更新"></a></li-->
 
 						<!--li><a href="http://tomonishintaku.com/blog/" onclick="ga('send','event','banner','click','blog', 1);"><img class="lazyload" src="/images/top/bnr_shintaku_blog.png" alt="現代美術家新宅睦仁の日々の徒然のエッセイやアート関連のコラムなどのブログ"></a></li-->
 
-						<li><a href="https://www.tricera.net/artist/8100604" target="_blank"><img class="lazyload" src="/images/top/bnr_tricera-en.png" alt="tricera, selling paintings art online"></a></li>
+						<li><a href="https://www.tricera.net/artist/8100604" target="_blank"><img class="lazyload" src="/images/top/bnr_tricera-en.png" alt="tricera, selling paintings art online" rel="noopener noreferrer"></a></li>
 
 						<!--li><a href="https://aonose.wixsite.com/aonose/" onclick="ga('send','event','banner','click','aonose', 1);" target="_blank"><img class="lazyload" src="/images/top/aonose-bnr.jpg" alt="現代美術初心者でも楽しめる現代アートのaonose"></a></li-->
-
-						<!--li><a class="lazyload"href="/works/writing.html"><img class="lazyload" src="/images/top/bnr_writing.png" alt="現代美術家新宅睦仁の、文章作成、文筆の仕事の記録"></a></li-->
 
 					</ul>
 
