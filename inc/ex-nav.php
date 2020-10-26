961dcc50aef2e203a918503728b922ca9058e195
tomonishintaku <info@shintaku.co>
2020-10-26 17:19:09 +0900

    re-initial commit

--- inc/ex-nav.php
+++ inc/ex-nav.php
@@ -1,54 +1,161 @@
-<div class="clearfix">
-	<h2 class="line">2018</h2>
-	<ul class="img_txt_1">
-				<li>
-			<a href="2018_colour-me-well.html"><img class="lazyload" alt="新宅睦仁シンガポール個展「COLOUR ME WELL」" border="0" src="/images/exhibition/list_img/2018-colour-me-well.jpg">
-			<p>2018/8/1-2018/8/21<br>
-			<span class="ttl"><span class="type">個展</span>COLOUR ME WELL</span> One East Asia Gallery/シンガポール</p><span class="sub">2018/個展「COLOUR ME WELL」＠シンガポール</span></a>
-		</li>
-		<!--li>
-			<a href="2018_lets-eat-everyone.html"><img class="lazyload" alt="Solo show, Let's Eat, Everyone" border="0" src="/images/exhibition/list_img/2018_lets-eat.jpg">
-			<p>未定Apr/16-May/13<br>
-			<span class="ttl"><span class="type">個展</span>みんなで食べよう</span> Artistry, シンガポール</p><span class="sub">2018/個展「みんなで食べよう」＠シンガポール</span></a>
-		</li-->
-	</ul>
-	<h2 class="line">2016</h2>
-	<ul class="img_txt_1">
-		<li>
-			<a href="161126_conveni-bento2.html"><img class="lazyload" alt="新宅睦仁個展「コンビニ弁当の山」" border="0" src="/images/exhibition/list_img/161126-mtcvbento-time-is-money.jpg">
-			<p>2016/11/26-2016/12/25<br>
-			<span class="ttl"><span class="type">個展</span>コンビニ弁当の山－Time is money.</span> トーキョーワンダーサイト渋谷/東京</p><span class="sub">2016/個展「コンビニ弁当の山－Time is money.」＠東京</span></a>
-		</li>
-		<li>
-			<a href="160112-cvbentonoyama.html"><img class="lazyload" alt="新宅睦仁個展「コンビニ弁当の山」" border="0" src="/images/exhibition/list_img/160112-cvbentonoyama.jpg">
-			<p>2016/1/12-2016/2/13<br>
-			<span class="ttl"><span class="type">個展</span>コンビニ弁当の山</span> 静岡市クリエーター支援センター（CCC）/静岡</p><span class="sub">2016/個展「コンビニ弁当の山」＠静岡</span></a>
-		</li>
-	</ul>
-	<h2 class="line">2015</h2>
-	<ul class="img_txt_1">
-		<li>
-			<a href="150907-cupnoodlenotaki.html"><img class="lazyload" alt="新宅睦仁個展「カップヌードルの滝」" border="0" src="/images/exhibition/list_img/150907-cupnoodlenotaki.jpg">
-			<p>2015/9/8-2015/9/27<br>
-			<span class="ttl"><span class="type">個展</span>カップヌードルの滝</span> HAGISO/東京</p><span class="sub">2015/個展「カップヌードルの滝」＠東京</span></a>
-		</li>
-		<li>
-			<a href="150612-taihaig.html"><img class="lazyload" alt="退廃藝術展2015" border="0" src="/images/exhibition/list_img/150612-taihaig.jpg">
-			<p>2015/6/12-2015/7/12<br>
-			<span class="ttl"><span class="type">グループ展</span>退廃藝術展2015</span> DESK/okumura/東京</p><span class="sub">2015/グループ展「退廃藝術展2015」＠東京</span></a>
-		</li>
-		<li>
-			<a href="150531-bjshokudo.html"><img class="lazyload" alt="美術食堂〜食べるところから新しい芸術は生まれる〜" border="0" src="/images/exhibition/list_img/150531-bjshokudo.jpg">
-			<p>2015/5/31-2015/6/28<br>
-			<span class="ttl"><span class="type">グループ展</span>美術食堂〜食べるところから新しい芸術は生まれる〜</span> ART SPACE ZERO-ONE/大阪</p><span class="sub">2015/グループ展「美術食堂」＠大阪</span></a>
-		</li>
-	</ul>
-	<h2 class="line">2014</h2>
-	<ul class="img_txt_1">
-		<li>
-			<a href="141002-gyudonnotaki.html"><img class="lazyload" alt="新宅睦仁個展「牛丼の滝」" border="0" src="/images/exhibition/list_img/141002-gyudonnotaki.jpg">
-			<p>2014/10/02-2014/10/14<br>
-			<span class="ttl"><span class="type">個展</span>牛丼の滝</span> 沢田マンションギャラリーroom38/高知</p><span class="sub">2014/個展「牛丼の滝」＠高知</span></a>
-		</li>
-	</ul>
+<div class="clearfix">
+	<h2 class="line">2020</h2>
+
+	<ul class="img_txt_1">
+
+		<li>
+			<a href="http://whiteboxnyc.org/event/good-trouble/"><img class="lazyload" alt="WhiteBox/ニューヨーク グループ展「GOOD TROUBLE」" border="0" src="/images/exhibition/list_img/good-trouble_2010.jpg">
+			<p>2020/10/17-2020/11/11<br>
+
+			<span class="ttl"><span class="type">グループ展</span>GOOD TROUBLE</span> WhiteBox/ニューヨーク</p><span class="sub">2020/グループ展「GOOD TROUBLE」＠ニューヨーク</span></a>
+		</li>
+
+		<li>
+			<a href="/blog/mind-the-gap_2020"><img class="lazyload" alt="THE blank GALLERY/東京 グループ展「MIND THE GAP」" border="0" src="/images/exhibition/list_img/mind-the-gap_2020.jpg">
+			<p>2020/9/19-2020/10/4<br>
+
+			<span class="ttl"><span class="type">グループ展</span>MIND THE GAP</span> THE blank GALLERY/東京</p><span class="sub">2020/グループ展「MIND THE GAP」＠東京</span></a>
+		</li>
+
+		<li>
+			<a href="/blog/satans-food-court_2019"><img class="lazyload" alt="Art Share L.A./ロサンゼルス グループ展「Satan's Food Court」" border="0" src="/images/exhibition/list_img/satans-food-court_2019.jpg">
+			<p>2020/4/27-2020/5/6<br>
+
+			<span class="ttl"><span class="type">グループ展</span>Satan's Food Court</span> Art Share L.A./ロサンゼルス</p><span class="sub">2020/グループ展「Satan's Food Court」＠ロサンゼルス</span></a>
+		</li>
+
+	</ul>
+
+	<h2 class="line">2019</h2>
+
+	<ul class="img_txt_1">
+
+		<li>
+			<a href="/blog/11th-annual-5050-show_2019"><img class="lazyload" alt="Sanchez Art Center/サンフランシスコ グループ展「11th Annual 50|50 Show」" border="0" src="/images/exhibition/list_img/11th-annual-5050-show_2019.jpg">
+			<p>2019/8/30-2019/9/22<br>
+
+			<span class="ttl"><span class="type">グループ展</span>11th Annual 50|50 Show</span> Sanchez Art Center/サンフランシスコ</p><span class="sub">2019/グループ展「11th Annual 50|50 Show」＠サンフランシスコ</span></a>
+		</li>
+
+		<li>
+			<a href="/blog/let-me-eat-cake-too_1906"><img class="lazyload" alt="Blue Roof Studios/ロサンゼルス グループ展「Let Me Eat Cake, Too」" border="0" src="/images/exhibition/list_img/let-me-eat-cake-too_1906.jpg">
+			<p>2019/6/20-2019/7/13<br>
+
+			<span class="ttl"><span class="type">グループ展</span>Let Me Eat Cake, Too</span> Blue Roof Studios/ロサンゼルス</p><span class="sub">2019/グループ展「Let Me Eat Cake, Too」＠ロサンゼルス</span></a>
+		</li>
+
+		<li>
+			<a href="/blog/kitchen-art-space-zero-point_1904"><img class="lazyload" alt="ART Space ZERO-POINT/東京 グループ展「KITCHEN」" border="0" src="/images/exhibition/list_img/kitchen-art-space-zero-point_1904.jpg">
+			<p>2019/4/27-2019/5/6<br>
+
+			<span class="ttl"><span class="type">グループ展</span>KITCHEN</span> ART Space ZERO-POINT/東京</p><span class="sub">2019/グループ展「KITCHEN」＠東京</span></a>
+		</li>
+
+	</ul>
+
+
+	<h2 class="line">2018</h2>
+
+	<ul class="img_txt_1">
+
+		<li>
+
+			<a href="2018_colour-me-well.html"><img class="lazyload" alt="新宅睦仁シンガポール個展「COLOUR ME WELL」" border="0" src="/images/exhibition/list_img/2018-colour-me-well.jpg">
+
+			<p>2018/8/1-2018/8/21<br>
+
+			<span class="ttl"><span class="type">個展</span>COLOUR ME WELL</span> One East Asia Gallery/シンガポール</p><span class="sub">2018/個展「COLOUR ME WELL」＠シンガポール</span></a>
+
+		</li>
+
+		<!--li>
+
+			<a href="2018_lets-eat-everyone.html"><img class="lazyload" alt="Solo show, Let's Eat, Everyone" border="0" src="/images/exhibition/list_img/2018_lets-eat.jpg">
+
+			<p>未定Apr/16-May/13<br>
+
+			<span class="ttl"><span class="type">個展</span>みんなで食べよう</span> Artistry, シンガポール</p><span class="sub">2018/個展「みんなで食べよう」＠シンガポール</span></a>
+
+		</li-->
+
+	</ul>
+
+	<h2 class="line">2016</h2>
+
+	<ul class="img_txt_1">
+
+		<li>
+
+			<a href="161126_conveni-bento2.html"><img class="lazyload" alt="新宅睦仁個展「コンビニ弁当の山」" border="0" src="/images/exhibition/list_img/161126-mtcvbento-time-is-money.jpg">
+
+			<p>2016/11/26-2016/12/25<br>
+
+			<span class="ttl"><span class="type">個展</span>コンビニ弁当の山－Time is money.</span> トーキョーワンダーサイト渋谷/東京</p><span class="sub">2016/個展「コンビニ弁当の山－Time is money.」＠東京</span></a>
+
+		</li>
+
+		<li>
+
+			<a href="160112-cvbentonoyama.html"><img class="lazyload" alt="新宅睦仁個展「コンビニ弁当の山」" border="0" src="/images/exhibition/list_img/160112-cvbentonoyama.jpg">
+
+			<p>2016/1/12-2016/2/13<br>
+
+			<span class="ttl"><span class="type">個展</span>コンビニ弁当の山</span> 静岡市クリエーター支援センター（CCC）/静岡</p><span class="sub">2016/個展「コンビニ弁当の山」＠静岡</span></a>
+
+		</li>
+
+	</ul>
+
+	<h2 class="line">2015</h2>
+
+	<ul class="img_txt_1">
+
+		<li>
+
+			<a href="150907-cupnoodlenotaki.html"><img class="lazyload" alt="新宅睦仁個展「カップヌードルの滝」" border="0" src="/images/exhibition/list_img/150907-cupnoodlenotaki.jpg">
+
+			<p>2015/9/8-2015/9/27<br>
+
+			<span class="ttl"><span class="type">個展</span>カップヌードルの滝</span> HAGISO/東京</p><span class="sub">2015/個展「カップヌードルの滝」＠東京</span></a>
+
+		</li>
+
+		<li>
+
+			<a href="150612-taihaig.html"><img class="lazyload" alt="退廃藝術展2015" border="0" src="/images/exhibition/list_img/150612-taihaig.jpg">
+
+			<p>2015/6/12-2015/7/12<br>
+
+			<span class="ttl"><span class="type">グループ展</span>退廃藝術展2015</span> DESK/okumura/東京</p><span class="sub">2015/グループ展「退廃藝術展2015」＠東京</span></a>
+
+		</li>
+
+		<li>
+
+			<a href="150531-bjshokudo.html"><img class="lazyload" alt="美術食堂〜食べるところから新しい芸術は生まれる〜" border="0" src="/images/exhibition/list_img/150531-bjshokudo.jpg">
+
+			<p>2015/5/31-2015/6/28<br>
+
+			<span class="ttl"><span class="type">グループ展</span>美術食堂〜食べるところから新しい芸術は生まれる〜</span> ART SPACE ZERO-ONE/大阪</p><span class="sub">2015/グループ展「美術食堂」＠大阪</span></a>
+
+		</li>
+
+	</ul>
+
+	<h2 class="line">2014</h2>
+
+	<ul class="img_txt_1">
+
+		<li>
+
+			<a href="141002-gyudonnotaki.html"><img class="lazyload" alt="新宅睦仁個展「牛丼の滝」" border="0" src="/images/exhibition/list_img/141002-gyudonnotaki.jpg">
+
+			<p>2014/10/02-2014/10/14<br>
+
+			<span class="ttl"><span class="type">個展</span>牛丼の滝</span> 沢田マンションギャラリーroom38/高知</p><span class="sub">2014/個展「牛丼の滝」＠高知</span></a>
+
+		</li>
+
+	</ul>
+
 </div>
