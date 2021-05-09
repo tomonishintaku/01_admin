76ccba0b26b893e1fd9b612dc58ac1919aa3a8f8
Tomoni Shintaku <info@shintaku.co>
2021-05-09 14:25:12 +0900

    Update JS accordion

--- en/blog/wp-content/themes/wpex-thoughts/footer.php
+++ en/blog/wp-content/themes/wpex-thoughts/footer.php
@@ -1,9 +1,10 @@
 <div class="clear"></div><!-- /clear any floats -->
+
 <?php include("../../inc/footer.php"); ?>
 
 <script>
-//共通jQuery本体を読み込んだ後に記述が必要のため以下に設置。
-//添削前の英文アコーディオン用
+	//共通jQuery本体を読み込んだ後に記述が必要のため以下に設置。
+	//添削前の英文アコーディオン用
 	$(function(){
 	$("#acMenu dt").on("click", function() {
 	$(this).next().slideToggle();
@@ -12,5 +13,6 @@
 </script>
 	
 <?php wp_footer(); ?>
+
 </body>
 </html>
