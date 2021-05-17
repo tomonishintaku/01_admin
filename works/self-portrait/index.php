64af663a14c64296c5c93a45bfc7ccac31b52299
Tomoni Shintaku <info@shintaku.co>
2021-05-17 15:06:48 +0900

    Update to display self-portrait sort by newest to oldest

--- works/self-portrait/index.php
+++ works/self-portrait/index.php
@@ -4,7 +4,6 @@
 	<!--include meta-->
 	<?php include("../../inc/meta.php"); ?><!--/include meta -->
 	<meta content="<?php require_once dirname(__FILE__).'/../../inc/short-concept.php'; echo $selfportrait_jp;?>" lang="ja" name="description">
-	<meta content="自画像,鉛筆画,セルフポートレート" lang="ja" name="keywords">
 	<title>自画像(2000-<?php echo date("Y"); ?>)｜現代美術家/新宅睦仁</title>
 	<link href="/css/works.css" rel="stylesheet" type="text/css">
 </head>
@@ -26,8 +25,23 @@
 三段階のforかwhileループで取得する。
 		　-->
 
+<style>
+.wrap__inner{
+	display: flex;
+	flex-wrap: wrap;
+}
+.wrap__inner__col{
+width: 25%;
+}
+.wrap__inner__col img{
+	width: 100%;
+	}
+	
+	</style>
+
+
       <article class="wrap">
-		  
+		  <section class="wrap__inner">	
 		  <?php
 		  $target_dir = dirname(__FILE__).'/../../images/works/self-portrait/';
 		  
@@ -36,9 +50,13 @@
 		  exec($command, $result);
 		  $result = $result[0] - 1;
 		  //for ($i=0; $i<=$result; $i++) { 
-		  for ($year = 2005; $year <= 2100; $year++) { 
-		  for ($month = 1; $month <= 12; $month++) { 
-		  for ($day = 1; $day <= 31; $day++) {  
+		  //for ($year = 2005; $year <= 2100; $year++) { 
+		  //for ($month = 1; $month <= 12; $month++) { 
+		  //for ($day = 1; $day <= 31; $day++) {  
+		  for ($year = 2100; $year >= 2005; $year--) { 
+		  for ($month = 12; $month >= 1; $month--) { 
+		  for ($day = 31; $day >= 1; $day--) {  
+		  
 		  $imageFile = $year.'-'.$month.'-'.$day;	  
 		  
 		  //画像ファイルの存在チェック
@@ -48,7 +66,7 @@
 		  
 		   ?>
 		  
-			  <figure>
+			  <figure class="wrap__inner__col">
 				  <img alt="現代美術家の新宅睦仁(シンタクトモニ)の<?php echo $year.'年'.$month.'月'.$day.'日' ?>の自画像/鉛筆ドローイング" data-src="/images/works/self-portrait/<?php echo $imageFile ?>.jpg" src="/images/common/white.gif" class="lazyload">
 				  <figcaption><time datetime="<?php echo $year.'/'.$month.'/'.$day ?>" itemprop="datepublished"><?php echo $year.'/'.$month.'/'.$day ?></time>
 				  <small><?php echo$year; ?>年 紙、鉛筆、30.5×30.5cm &#12316 30.5×30.5cm</small>
@@ -69,7 +87,7 @@
 		  <?php }}}
 	  
 	  //} ?>
-		  
+		  </section>		  
 	  </article>
 		  
 		<section id="works-nav">
