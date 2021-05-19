81edc884a6ed1ceedf155cf9ab0427c7070cbe01
Tomoni Shintaku <info@shintaku.co>
2021-05-19 10:23:32 +0900

    updatecontact info delete Whatsup add Line

--- en/inc/contact.php
+++ en/inc/contact.php
@@ -1,6 +1,11 @@
 <ul>
+
 <li><a class="button email" href="mailto:&#105;&#110;&#102;&#111;&#64;&#116;&#111;&#109;&#111;&#110;&#105;&#115;&#104;&#105;&#110;&#116;&#97;&#107;&#117;&#46;&#99;&#111;&#109;"><i class="fas fa-envelope-square"></i> &#105;&#110;&#102;&#111;&#64;&#116;&#111;&#109;&#111;&#110;&#105;&#115;&#104;&#105;&#110;&#116;&#97;&#107;&#117;&#46;&#99;&#111;&#109;</a></li>
-<li><a class="button whatsapp" href="tel:3103577340" target="_blank" title="Click to call"><i class="fab fa-whatsapp-square"></i> 310 3577 340</a></li>
+
+<!--li><a class="button whatsapp" href="tel:3103577340" target="_blank" title="Click to call"><i class="fab fa-whatsapp-square"></i> 310 3577 340</a></li-->
+
+<li><a class="button whatsapp" href="https://line.me/ti/p/pyOsznXbUf" target="_blank" title="Click to call"><i class="fab fa-line"></i> @tshintaku</a></li>
+
 <li><a class="button twitter" href="https://twitter.com/tomoni_en" target="_blank" title="Click to share this post on Twitter"><i class="fab fa-twitter-square" aria-hidden="true"></i> @tomoni_en</a></li>
 
 <li> <a class="button facebook" href="https://www.facebook.com/tomoni.shintaku" target="_blank"><i class="fab fa-facebook-square" aria-hidden="true"></i> @tomoni.shintaku</a></li>
--- inc/contact.php
+++ inc/contact.php
@@ -1,11 +1,17 @@
-<ul>
-<li><a class="button email" href="mailto:&#105;&#110;&#102;&#111;&#64;&#116;&#111;&#109;&#111;&#110;&#105;&#115;&#104;&#105;&#110;&#116;&#97;&#107;&#117;&#46;&#99;&#111;&#109;"><i class="fas fa-envelope-square"></i> &#105;&#110;&#102;&#111;&#64;&#116;&#111;&#109;&#111;&#110;&#105;&#115;&#104;&#105;&#110;&#116;&#97;&#107;&#117;&#46;&#99;&#111;&#109;</a></li>
-<li><a class="button whatsapp" href="tel:3103577340" target="_blank" title="Click to call"><i class="fab fa-whatsapp-square"></i> 310 3577 340</a></li>
-<li><a class="button twitter" href="https://twitter.com/tomonishintaku" target="_blank" title="Click to share this post on Twitter"><i class="fab fa-twitter-square" aria-hidden="true"></i> @tomonishintaku</a></li>
-
-<li> <a class="button facebook" href="https://www.facebook.com/tomoni.shintaku" target="_blank"><i class="fab fa-facebook-square" aria-hidden="true"></i> @tomoni.shintaku</a></li>
-
-<li> <a class="button instagram" href="https://www.instagram.com/tomonishintaku/" target="_blank" title="Click to share this post on Instagram"><i class="fab fa-instagram" aria-hidden="true"></i> @tomonishintaku</a></li>
-
-<li> <a class="button linkedin" href="https://www.linkedin.com/in/tomonishintaku/" target="_blank" title="Click to share this post on LinkedIn"><i class="fab fa-linkedin" aria-hidden="true"></i> Tomoni Shintaku</a></li>
+<ul>
+
+<li><a class="button email" href="mailto:&#105;&#110;&#102;&#111;&#64;&#116;&#111;&#109;&#111;&#110;&#105;&#115;&#104;&#105;&#110;&#116;&#97;&#107;&#117;&#46;&#99;&#111;&#109;"><i class="fas fa-envelope-square"></i> &#105;&#110;&#102;&#111;&#64;&#116;&#111;&#109;&#111;&#110;&#105;&#115;&#104;&#105;&#110;&#116;&#97;&#107;&#117;&#46;&#99;&#111;&#109;</a></li>
+
+<!--li><a class="button whatsapp" href="tel:3103577340" target="_blank" title="Click to call"><i class="fab fa-whatsapp-square"></i> 310 3577 340</a></li-->
+
+<li><a class="button whatsapp" href="https://line.me/ti/p/pyOsznXbUf" target="_blank" title="Click to call"><i class="fab fa-line"></i> @tshintaku</a></li>
+
+<li><a class="button twitter" href="https://twitter.com/tomonishintaku" target="_blank" title="Click to share this post on Twitter"><i class="fab fa-twitter-square" aria-hidden="true"></i> @tomonishintaku</a></li>
+
+<li> <a class="button facebook" href="https://www.facebook.com/tomoni.shintaku" target="_blank"><i class="fab fa-facebook-square" aria-hidden="true"></i> @tomoni.shintaku</a></li>
+
+<li> <a class="button instagram" href="https://www.instagram.com/tomonishintaku/" target="_blank" title="Click to share this post on Instagram"><i class="fab fa-instagram" aria-hidden="true"></i> @tomonishintaku</a></li>
+
+<li> <a class="button linkedin" href="https://www.linkedin.com/in/tomonishintaku/" target="_blank" title="Click to share this post on LinkedIn"><i class="fab fa-linkedin" aria-hidden="true"></i> Tomoni Shintaku</a></li>
+
 </ul>
