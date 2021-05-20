016cebed479130d47918b7269113457ce93da7ea
Tomoni Shintaku <info@shintaku.co>
2021-05-20 18:06:11 +0900

    Update to diplay unique page's title for OGP tag for English

--- en/inc/meta.php
+++ en/inc/meta.php
@@ -1,3 +1,23 @@
+<?php
+// Picking up the page title 
+// URL を指定する
+$url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+// 指定された URL から HTML 文字列を取得
+$html = file_get_contents($url);
+
+// 内部エンコーディングに指定している文字コードに変換
+$html = mb_convert_encoding($html, mb_internal_encoding(), "auto" );
+
+// 正規表現を使って title タグを抜き出す
+if ( preg_match( "/<title>(.*?)<\/title>/i", $html, $matches) ) {
+    $page_title = $matches[1];
+} else {
+    $page_title = 'Contemporary Artist Tomoni Shintaku';
+}
+
+?>
+
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-34577324-1"></script>
 <script>
@@ -13,7 +33,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-59FKPF7');</script>
 <!-- End Google Tag Manager -->
-<meta charset="UTF-8"><meta name="viewport" content="width=device-width"><meta name="author" content="Tomoni Shintaku"/><meta name="copyright" content="Tomoni Shintaku"/><meta property="og:title" content="Contemporary Artist Tomoni Shintaku"/><meta property="og:type" content="website"/><meta property="og:image" content="https://tomonishintaku.com/images/common/og-image.png"/><meta property="og:url" content="https://tomonishintaku.com<?php echo $_SERVER['PHP_SELF'] ?>"/><meta property="og:description" content="This website introduces Contemporary Artist Tomoni Shintaku’s artworks, which consist of painting and installation. It will also announce his Solo Show and Group Exhibition."/><meta property="og:site_name" content="Contemporary Artist Tomoni Shintaku"/><meta name="twitter:card" content="summary"><meta name="twitter:site" content="@tomoni_en"><meta name="twitter:title" content="Contemporary Artist Tomoni Shintaku"><meta name="twitter:description" content="This website introduces Contemporary Artist Tomoni Shintaku’s artworks, which consist of painting and installation. It will also announce his  Solo Show and Group Exhibition."><meta name="twitter:image" content="https://tomonishintaku.com/images/common/og-image.png"><meta property="og:locale" content="en_US" /><meta property="fb:app_id" content="210438150129981" /><link rel="shortcut icon" href="/images/common/favicon.ico" type="/image/vnd.microsoft.icon"><link rel="icon" href="/images/common/favicon.ico" type="/image/vnd.microsoft.icon"><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"><link rel="alternate" hreflang="en-us" href="https://tomonishintaku.com<?php echo $_SERVER['PHP_SELF'] ?>" /><link rel="alternate" hreflang="ja" href="https://tomonishintaku.com<?php $str = $_SERVER['PHP_SELF']; $str = str_replace('/en', '', $str); echo $str;?>" />
+<meta charset="UTF-8"><meta name="viewport" content="width=device-width"><meta name="author" content="Tomoni Shintaku"/><meta name="copyright" content="Tomoni Shintaku"/><meta property="og:title" content='<?php echo $page_title; ?>'/><meta property="og:type" content="website"/><meta property="og:image" content="https://tomonishintaku.com/images/common/og-image.png"/><meta property="og:url" content="https://tomonishintaku.com<?php echo $_SERVER['PHP_SELF'] ?>"/><meta property="og:description" content="This website introduces Contemporary Artist Tomoni Shintaku’s artworks, which consist of painting and installation. It will also announce his Solo Show and Group Exhibition."/><meta property="og:site_name" content="Contemporary Artist Tomoni Shintaku"/><meta name="twitter:card" content="summary"><meta name="twitter:site" content="@tomoni_en"><meta name="twitter:title" content='<?php echo $page_title; ?>'><meta name="twitter:description" content="This website introduces Contemporary Artist Tomoni Shintaku’s artworks, which consist of painting and installation. It will also announce his  Solo Show and Group Exhibition."><meta name="twitter:image" content="https://tomonishintaku.com/images/common/og-image.png"><meta property="og:locale" content="en_US" /><meta property="fb:app_id" content="210438150129981" /><link rel="shortcut icon" href="/images/common/favicon.ico" type="/image/vnd.microsoft.icon"><link rel="icon" href="/images/common/favicon.ico" type="/image/vnd.microsoft.icon"><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"><link rel="alternate" hreflang="en-us" href="https://tomonishintaku.com<?php echo $_SERVER['PHP_SELF'] ?>" /><link rel="alternate" hreflang="ja" href="https://tomonishintaku.com<?php $str = $_SERVER['PHP_SELF']; $str = str_replace('/en', '', $str); echo $str;?>" />
 
 <?php require_once('/home/c8378821/public_html/tomonishintaku.com/inc/common/functions.php'); ?>
 
