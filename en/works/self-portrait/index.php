528dabf63eac4cd55f3dd9c983c516fb175e3533
tomonishintaku <dev@shintaku.co>
2022-06-20 08:03:08 +0200

    Update css and add loading loay

--- en/works/self-portrait/index.php
+++ en/works/self-portrait/index.php
@@ -6,6 +6,57 @@
 	<meta content="abortion, pro-life, pro-choice, Catholic" lang="en" name="keywords">
 	<title>Self-portrait(2004-<?php echo date("Y"); ?>)｜Contemporary artist SHINTAKU Tomoni</title>
 	<link href="/css/works.css" rel="stylesheet" type="text/css">
+		<style>
+/*最終的にcssファイルに移行予定*/
+.wrap__inner {
+	display: flex;
+	flex-wrap: wrap;
+}
+.inner__col {
+	width: 24.85%;
+	margin-right: 0.2%;
+}
+.inner__col:nth-child(4n) {
+	margin-right: 0;
+}
+.inner__col img {
+	width: 100%;
+}
+.col__caption {
+	text-align: center;
+	font-size: 0.75rem;
+	margin-top: 0;
+	display: block;
+	color: #777;
+	padding-bottom: 0.5rem;
+}
+@media only screen and (min-width: 769px) {
+.self-portrait-image-wrap__inner{
+ width: 254px;
+ height: 374px;
+	}
+}
+@media only screen and (max-width: 768px) {
+	.self-portrait-image-wrap__inner{
+ 		width: 114px;
+ 		height: 168px;
+	}
+	.inner__col {
+		width: 32%;
+		margin-right: 1.5%;
+	}
+	.inner__col {
+		width: 33.133333333333333%;
+		margin-right: 0.2%;
+	}
+	.inner__col:nth-child(3n) {
+		margin-right: 0;
+	}
+	.inner__col:nth-child(4n) {
+		margin-right: 0.2%;
+	}
+}
+</style>
 </head>
 <body>
 	<?php include("../../../inc/header.php"); ?>
@@ -14,56 +65,6 @@
 		<!-- 自画像プロジェクトの開始年月日 2004.03.03 16:28　-->
 		<h1 class="line">Self-portrait(2004-<?php echo date("Y"); ?>)</h1>
         <p class="details">paper, pencils, colored pencils, watercolors, dermatographs, coffee, felt-tip pens, ballpoint pens, etc. 6.2 × 8.3 inches (15.8 × 21.0 cm) &#12316 9 × 12 inches (22.9 × 30.5 cm)</p>
-
-<style>
-/*最終的にcssファイルに移行予定*/
-
-.wrap__inner{
-    display: flex;
-    flex-wrap: wrap;
-}
-.inner__col{
-width: 24.85%;
-margin-right: 0.2%;
-}
-
-.inner__col:nth-child(4n){
-    margin-right: 0;
-    }
-
-.inner__col img{
-    width: 100%;
-    }
-.col__caption{
-    text-align: center;
-    font-size: 0.75rem;
-    margin-top: 0;
-    display: block;
-    color: #777;
-    padding-bottom: 0.5rem;
-}
-@media only screen and (max-width: 768px){
-    .inner__col{
-     width: 32%;
-     margin-right: 1.5%;
-        }
-        .inner__col{
-             width: 33.133333333333333%;
-             margin-right: 0.2%;
-                }
-                
-                .inner__col:nth-child(3n){
-margin-right: 0;
-                    
-                }
-                .inner__col:nth-child(4n){
-                    margin-right: 0.2%;
-                    }
-                
-    }
-	</style>
-
-
       <article class="wrap">
 		  <section class="wrap__inner">	
 		  <?php
@@ -88,9 +89,9 @@ margin-right: 0;
 			  // START images exist
 		   ?>
 		  
-			  <figure class="inner__col">
-				  <a href="/images/works/self-portrait/<?php echo $imageFile ?>@2x.jpg" class="zoom">
-                      <img alt="Contemporary artist SHINTAKU Tomoni's <?php echo $year.'/'.$month.'/'.$day ?> Self-portrait or pencil drawing" data-src="/images/works/self-portrait/<?php echo $imageFile ?>.jpg" src="/images/common/white.gif" class="lazyload">
+			  <figure class="inner__col self-portrait-image-wrap">
+				  <a href="/images/works/self-portrait/<?php echo $imageFile ?>@2x.jpg" class="zoom self-portrait-image-wrap__inner">
+                      <img alt="Contemporary artist SHINTAKU Tomoni's <?php echo $year.'/'.$month.'/'.$day ?> Self-portrait or pencil drawing" data-src="/images/works/self-portrait/<?php echo $imageFile ?>.jpg" src="/images/common/white.gif" class="lazyload" loading="lazy">
                   </a>
 				  <figcaption class="col__caption"><time datetime="<?php echo $year.'-'.$month.'-'.$day ?>" itemprop="datepublished"><?php echo $year.'.'.$month.'.'.$day ?></time>
 				  <!-- small><?php echo$year; ?>年 紙、鉛筆、30.5×30.5cm &#12316 30.5×30.5cm</small-->
